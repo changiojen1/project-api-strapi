db25417aa97a2192d6d0f4cb34796eefe047b959
changiojen1 <096253@jyes.ntpc.edu.tw>
2021-06-15 19:34:24 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7e729618612ea5dce202ad4659f54dba'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4f7b472b-25a0-472f-9a1c-43fc45ab51fd'
+};
