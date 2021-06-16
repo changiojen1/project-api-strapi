5ba75aff3911720f834285f347ed2fe909617a79
changiojen1 <096253@jyes.ntpc.edu.tw>
2021-06-16 19:24:10 +0800

    Change the title of /crown page to 'Crown Clothing 2021'

--- api/events/controllers/events.js
+++ api/events/controllers/events.js
@@ -5,4 +5,21 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    homepage: async (ctx) => {
+        let entities;
+        try{
+            entities = await strapi.services.category.find({
+                _sort: "cid:asc"});
+            console.log("entities1",entities);
+
+            return await ctx.render("crown2_xx",{
+                data:entities,
+                title:"Crown Clothing 2021",
+            })
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+};
